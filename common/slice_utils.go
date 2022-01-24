8ab74deb76a93d1f0025734ce538021af7e813b0
Jaeyo <jaeyo.developer@gmail.com>
2022-01-24 12:09:51 +0900

    contains

--- common/slice_utils.go
+++ common/slice_utils.go
@@ -1,40 +1,21 @@
 package common
 
-func HasInt(s []int, value int) bool {
-	for _, v := range s {
-		if v == value {
-			return true
-		}
-	}
-	return false
-}
-
-func RemoveInt(s []int, value int) []int {
-	var result []int
-	for _, v := range s {
-		if v != value {
-			result = append(result, v)
-		}
-	}
-	return result
-}
 
 type Int64s []int64
 
 func (i Int64s) Contain(v int64) bool {
-	for _, item := range i {
-		if item == v {
-			return true
-		}
-	}
-	return false
+	return contains[int64](i, v)
 }
 
 type Strings []string
 
 func (s Strings) Contain(v string) bool {
-	for _, item := range s {
-		if item == v {
+	return contains[string](s, v)
+}
+
+func contains[T any](a []T, b T) bool {
+	for _, item := range a {
+		if item == b {
 			return true
 		}
 	}
